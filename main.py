caeee9b1c0595b20dc994c1101934fdde56149e9
sonbarse17 <sushantsonbarse90@gmail.com>
2026-03-11 16:08:01 +0530

    Introduce high-entropy fake token to force Gitleaks pipeline crash

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ def subtract(a, b): return a - b
 def multiply(a, b): return a * b
 
 # INTENTIONAL VULNERABILITY 1: Hardcoded Secret
-GITHUB_TOKEN = "ghp_1234567890abcdefghijklmnopqrstuvwxyza"
+# Gitleaks uses Shannon Entropy. Low-entropy strings (like 12345...) are ignored as false positives.
+# This is a high-entropy fake token that will force Gitleaks to explode.
+GITHUB_TOKEN = "ghp_8XoY2aB6k9Z1wQ0eR7tP4uI3lA5sD2fG1hJ4"
 
 # INTENTIONAL VULNERABILITY 2 & 3: Command Injection & SQL Injection
 from flask import Flask, request
